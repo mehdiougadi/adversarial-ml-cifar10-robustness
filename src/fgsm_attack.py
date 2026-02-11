97f238df2914249e4c83437d0b369b2453469568
Mehdi <mehdiougadiofficial@gmail.com>
2026-02-11 12:45:56 -0500

    Mehdi: defined methods needed for fgsm_attack.py file

---
+++ src/fgsm_attack.py
@@ -0,0 +1,26 @@
+def fgsm_attack():
+    pass
+
+
+def generate_adversarial_examples():
+    pass
+
+
+def evaluate_attack():
+    pass
+
+
+def visualize_adversarial_examples():
+    pass
+
+
+def plot_attack_strength_analysis():
+    pass
+
+
+def save_fgsm_results():
+    pass
+
+
+def run_fgsm_attack():
+    pass
--- src/model.py
+++ src/model.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 class SimpleCNN(nn.Module):
+
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
