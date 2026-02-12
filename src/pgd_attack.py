ac92313fd6cc2381d9136608a0060de14f8da8e1
Mehdi <mehdiougadiofficial@gmail.com>
2026-02-12 11:25:18 -0500

    Mehdi: defined the methods needed for pgd_attacks.py

---
+++ src/pgd_attack.py
@@ -0,0 +1,36 @@
+
+
+def pgd_attack():
+    pass
+
+
+def generate_pgd_adversarial_examples():
+    pass
+
+
+def evaluate_pgd_attack():
+    pass
+
+
+def visualize_pgd_examples():
+    pass
+
+
+def plot_pgd_analysis():
+    pass
+
+
+def compare_fgsm_pgd():
+    pass
+
+
+def save_pgd_results():
+    pass
+
+
+def save_comparison_results():
+    pass
+
+
+def run_pgd_attack():
+    pass
